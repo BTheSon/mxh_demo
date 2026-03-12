93d7988e8eff09e2b5dd5f4b110069586375abe9
Bui_The_Son <buitheson201@gmail.com>
2026-03-12 21:58:30 +0700

    init comit

---
+++ src/main/java/vn/qnu/edu/mxh_demo/MxhDemoApplication.java
@@ -0,0 +1,13 @@
+package vn.qnu.edu.mxh_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxhDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxhDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/qnu/edu/mxh_demo/MxhDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.qnu.edu.mxh_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxhDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
