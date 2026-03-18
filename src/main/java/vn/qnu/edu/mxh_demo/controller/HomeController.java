e28524e5dcf34756a8d3e6eb2d6b6a7b6900948d
Bui_The_Son <buitheson201@gmail.com>
2026-03-18 20:20:45 +0700

    wẻtyu

--- src/main/java/vn/qnu/edu/mxh_demo/controller/HomeController.java
+++ src/main/java/vn/qnu/edu/mxh_demo/controller/HomeController.java
@@ -1,13 +1,16 @@
 package vn.qnu.edu.mxh_demo.controller;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import vn.qnu.edu.mxh_demo.models.User;
+import vn.qnu.edu.mxh_demo.services.PostService;
 
 @Controller
 public class HomeController {
+    @Autowired private PostService postService;
 
     @GetMapping("/")
     public String index(HttpSession session, Model model) {
@@ -15,7 +18,11 @@ public class HomeController {
         if (user == null) {
             return "redirect:/login";
         }
+        var listFeed = postService.getPostsByUserId((Integer) user.getId());
+
         model.addAttribute("user", user);
+        model.addAttribute("my_feed", listFeed);
+
         return "index";
     }
 }
